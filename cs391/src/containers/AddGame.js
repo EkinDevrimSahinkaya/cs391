05245eaad800c618be9cbdfe6b1fe93cedfb40d6
bsknblc <basak.balci@ozu.edu.tr>
2022-05-29 16:50:16 +0300

    data

--- cs391/src/containers/AddGame.js
+++ cs391/src/containers/AddGame.js
@@ -1,10 +1,17 @@
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import { useState } from 'react';
+import 'react-dropdown/style.css';
 
-const options = [
+const publishers = [
   'one', 'two', 'three'
 ];
-const defaultOption = options[0];
+
+const genres =[
+    'Sandbox', 'Real-time strategy (RTS)', 'Shooters (FPS and TPS)', 'Multiplayer online battle arena (MOBA)',
+    'Role-playing (RPG, ARPG, and More)', 'Simulation and sports', 'Puzzlers and party games', 'Action-adventure',
+    'Survival and horror', 'Platformer'
+];
 
 export default function AddGame() {
     return (
@@ -12,9 +19,14 @@ export default function AddGame() {
             <form>
                 <label>Name: <input type="text" name = "name" /></label>
                 <label>Publisher: 
-                    <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;    
+                    <Dropdown options={publishers} onChange={this._onSelect} value={publishers[0]} placeholder="Select an option" />;    
                 </label>
-                
+                <label>Game Genre: 
+                    <Dropdown options={genres} onChange={this._onSelect} value={genres[0]} placeholder="Select an option" />;    
+                </label>
+                <label>Price: 
+                    <input type="number" name = "price" />  
+                </label>
                 < input type = "submit" value = "Submit" />
             </form>
         </main>
