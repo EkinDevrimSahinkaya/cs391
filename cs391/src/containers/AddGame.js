3d01234de21f9cb07383073c3d8ef7816e7112f7
bsknblc <basak.balci@ozu.edu.tr>
2022-05-29 15:28:14 +0300

    basic add game

--- cs391/src/containers/AddGame.js
+++ cs391/src/containers/AddGame.js
@@ -1,11 +1,20 @@
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const options = [
+  'one', 'two', 'three'
+];
+const defaultOption = options[0];
+
 export default function AddGame() {
     return (
         <main style={{ padding: "1rem 0" }}>
             <form>
-                <label>
-                Name:
-                <input type="text" name = "name" />
+                <label>Name: <input type="text" name = "name" /></label>
+                <label>Publisher: 
+                    <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;    
                 </label>
+                
                 < input type = "submit" value = "Submit" />
             </form>
         </main>
