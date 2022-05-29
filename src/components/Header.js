5265424d253f4d09ecf9543d3cad6a920a107b84
Ekin Devrim Şahinkaya <devrim.sahinkaya@ozu.edu.tr>
2022-05-29 12:00:23 +0300

    start

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = (props) => {
+    const {title} = props;
+    return <div> {title} </div>
+ }
+
+ export default Header;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Products from "./containers/Products";
+import Contact from "./containers/Contact";
+import About from "./containers/About";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="products" element={<Products />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
