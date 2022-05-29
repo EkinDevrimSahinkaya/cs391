58ac6b67ea4d2d9f934d5bb9669111077eab8b7a
bsknblc <basak.balci@ozu.edu.tr>
2022-05-29 13:55:01 +0300

    contact

--- cs391/src/containers/Contact.js
+++ cs391/src/containers/Contact.js
@@ -1,15 +1,21 @@
+import map from './images/map.jpg'
+import style from './css/style.css'
+
 export default function Contact() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Contact Information</h2>
-        <b>Object Shop  Limited</b>
-        <p>21 Old Gloucester Street</p>
-        <p>London, WC1N 37AD</p>
-        <p>United Kingdom</p>
+        <b>Çekmeköy Campus Nişantepe District,</b>
+        <p>Orman Street, 34794</p>
+        <p>Çekmeköy - İstanbul</p>
+        <p>info@ozyegin.edu.tr</p>
   
-        <b>For Personal users, call us at:</b> 0800 083 3492
+        <b>Call us at:</b> +90(216) 564 90 00
         <br />
-        <b>For Entreprise users, call us at:</b> 0800 141 6076
+        <b> Fax us at: </b> +90(216) 564 99 99
+        <div>
+          <img id="map" style={style} src={map} alt="Map" />;<img />
+        </div>
       </main>
     );
   }
