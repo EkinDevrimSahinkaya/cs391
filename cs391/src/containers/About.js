20e7337cb1f6af725e63f1b90e8b2d8d32d302ac
bsknblc <basak.balci@ozu.edu.tr>
2022-05-29 14:24:46 +0300

    about us

--- cs391/src/containers/About.js
+++ cs391/src/containers/About.js
@@ -6,23 +6,15 @@ import style from './css/style.css'
 export default function About() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Contact Information</h2>
-        <b>Çekmeköy Campus Nişantepe District,</b>
-        <p>Orman Street, 34794</p>
-        <p>Çekmeköy - İstanbul</p>
-        <p>info@ozyegin.edu.tr</p>
-  
-        <b>Call us at:</b> +90(216) 564 90 00
-        <br />
-        <b> Fax us at: </b> +90(216) 564 99 99
+        <h2>About Us</h2>
+        <b>Team</b>
+        <p>Ekin Devrim Şahinkaya</p>
+        <p>Ceren Yıldızdoğan</p>
+        <p>Başak Balcı</p>
         <div>
-          <img id="ekin" style={style} src={ekin} alt="Ekin" />;<img />
-        </div>
-        <div>
-          <img id="ceren" style={style} src={ceren} alt="Ceren" />;<img />
-        </div>
-        <div>
-          <img id="basak" style={style} src={basak} alt="Başak" />;<img />
+            <img id="ekin" className={`aboutImg`} style={style} src={ekin} alt="Ekin" />;<img />
+            <img id="ceren" className={`aboutImg`} style={style} src={ceren} alt="Ceren" />;<img />
+            <img id="basak" className={`aboutImg`} style={style} src={basak} alt="Başak" />;<img />
         </div>
       </main>
     );
