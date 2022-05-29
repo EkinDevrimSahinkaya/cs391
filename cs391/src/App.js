64ca655b0ee14dfd6ed8eb080a14e08d330cc767
Ekin Devrim Şahinkaya <devrim.sahinkaya@ozu.edu.tr>
2022-05-29 12:56:26 +0300

    changes

--- cs391/src/App.js
+++ cs391/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Store</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        {/* <Link to="/products">Products</Link> | <Link to="/about">About</Link>|{" "} */}
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;
---
+++ cs391/src/containers/Contact.js
@@ -0,0 +1,15 @@
+export default function Contact() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Contact Information</h2>
+        <b>Object Shop  Limited</b>
+        <p>21 Old Gloucester Street</p>
+        <p>London, WC1N 37AD</p>
+        <p>United Kingdom</p>
+  
+        <b>For Personal users, call us at:</b> 0800 083 3492
+        <br />
+        <b>For Entreprise users, call us at:</b> 0800 141 6076
+      </main>
+    );
+  }
